45044b7da63e312fd6a6a401f591b96f36e7d82a
Serhieie <89160714+Serhieie@users.noreply.github.com>
2023-10-08 06:36:07 +0300

    fixed obj

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,42 @@
 import throttle from 'lodash.throttle';
 import Notiflix from 'notiflix';
 
+const FEED_BACK_MESSAGE = 'feed back';
+const EMAIL_VALUE = 'email';
+
 const refs = {
-  FEED_BACK_MESSAGE: 'feed back msg',
-  EMAIL_VALUE: 'email',
   form: document.querySelector('.feedback-form'),
   textarea: document.querySelector('.feedback-form textarea'),
   email: document.querySelector('.feedback-form input'),
 };
 
-const formData = {};
+const { form, textarea, email } = refs;
 
-refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(onInput, 500));
+let formData = {};
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInput, 500));
+
+function output(evt) {
+  const savedMsg = localStorage.getItem(FEED_BACK_MESSAGE);
+  const savedEmail = localStorage.getItem(EMAIL_VALUE);
+  if (savedEmail) {
+    email.value = savedEmail;
+    formData[EMAIL_VALUE] = savedEmail;
+  }
+  if (savedMsg) {
+    textarea.value = savedMsg;
+    formData[FEED_BACK_MESSAGE] = savedMsg;
+  }
+}
+
+output();
 
 function onInput(evt) {
   const target = evt.target;
-  if (target === refs.email) {
+  if (target === email) {
     onEmailInput(evt);
-  } else if (target === refs.textarea) {
+  } else if (target === textarea) {
     onTextAreaInput(evt);
   }
 }
@@ -26,35 +44,23 @@ function onInput(evt) {
 function onEmailInput(evt) {
   formData[evt.target.name] = evt.target.value;
   const emailValue = evt.target.value;
-  localStorage.setItem(refs.EMAIL_VALUE, emailValue);
+  localStorage.setItem(EMAIL_VALUE, emailValue);
 }
 
 function onTextAreaInput(evt) {
   formData[evt.target.name] = evt.target.value;
   const value = evt.target.value;
-  localStorage.setItem(refs.FEED_BACK_MESSAGE, value);
+  localStorage.setItem(FEED_BACK_MESSAGE, value);
 }
 
-function output(evt) {
-  const savedMsg = localStorage.getItem(refs.FEED_BACK_MESSAGE);
-  const savedEmail = localStorage.getItem(refs.EMAIL_VALUE);
-  if (savedMsg) {
-    refs.textarea.value = savedMsg;
-  }
-  if (savedEmail) {
-    refs.email.value = savedEmail;
-  }
-}
 function onFormSubmit(evt) {
   evt.preventDefault();
-  if (refs.email.value === '' || refs.textarea.value === '') {
+  if (email.value === '' || textarea.value === '') {
     return Notiflix.Notify.failure('Заповніть усі поля форми');
   }
-  evt.target.reset();
-  console.log('Local Storage obj: ', localStorage);
-  localStorage.removeItem(refs.FEED_BACK_MESSAGE);
-  localStorage.removeItem(refs.EMAIL_VALUE);
   console.log(formData);
+  localStorage.removeItem(FEED_BACK_MESSAGE);
+  localStorage.removeItem(EMAIL_VALUE);
+  evt.target.reset();
+  formData = {};
 }
-
-output();
